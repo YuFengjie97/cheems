28eb6b5d3baa0fd851cc7d08ddefe324b235587a
Yu Fengjie <wb_yufengjie@meituan.com>
2022-06-24 11:02:05 +0800

    feat: selectArea

--- src/utils/selectArea.js
+++ src/utils/selectArea.js
@@ -1,19 +1,106 @@
-export function select(con) {
-  let conDom
-  if (typeof con === 'string') {
-    document.querySelector(con)
-  }else{
-    conDom = con
+export class SA {
+  isDown = false
+  con = null
+  dataKey = ''
+  start = [] // 0-->x,1-->y
+  end = []
+  area = null
+  selectData = []
+  mousedownCallback = () => {}
+  selectCallback = () => {}
+  mouseupCallback = () => {}
+
+  constructor(con, selectItem, dataKey, mousedownCallback, selectCallback, mouseupCallback) {
+    this.con = con
+    this.con.style.position = 'relative'
+    this.selectItem = selectItem
+    this.dataKey = dataKey
+    if (mousedownCallback) this.mousedownCallback = mousedownCallback
+    if (selectCallback) this.selectCallback = selectCallback
+    if (mouseupCallback) this.mouseupCallback = mouseupCallback
+    this.mousedown()
+    this.mousemove()
+    this.mouseup()
   }
-  conDom.style.position = 'relative'
+  mousedown() {
+    this.con.addEventListener('mousedown', (e) => {
+      this.isDown = true
+      console.log('mousedown')
+      this.start = [e.clientX, e.clientY]
+      this.createArea()
+      this.mousedownCallback()
+    })
+  }
 
-  const selectMask = document.createElement('div')
-  selectMask.setAttribute('class', 'mask')
+  mousemove() {
+    this.con.addEventListener('mousemove', (e) => {
+      if (!this.isDown) return
+      console.log('mousemove')
+      this.end = [e.clientX, e.clientY]
+      this.updateArea()
+      this.select()
+    })
+  }
 
-  // 添加选择遮罩
-  conDom.append(selectMask)
+  mouseup() {
+    this.con.addEventListener('mouseup', () => {
+      this.isDown = false
+      console.log('mouseup')
+      this.removeArea()
+      this.mouseupCallback()
+    })
+  }
 
-  conDom.addEventListener('click', () => {
-    console.log('click')
-  })
+  createArea() {
+    this.area = document.createElement('div')
+    this.area.style.position = 'absolute'
+    this.area.style.border = '1px solid rgb(0,119,255)'
+    this.area.style.background = 'rgba(0,119,255,0.2)'
+    this.con.appendChild(this.area)
+  }
+
+  removeArea() {
+    this.area.remove()
+  }
+
+  updateArea() {
+    let left = Math.min(this.start[0], this.end[0])
+    let top = Math.min(this.start[1], this.end[1])
+    let width = Math.abs(this.end[0] - this.start[0])
+    let height = Math.abs(this.end[1] - this.start[1])
+
+    this.area.style.left = `${left}px`
+    this.area.style.top = `${top}px`
+    this.area.style.width = `${width}px`
+    this.area.style.height = `${height}px`
+  }
+
+  // rect为getBoundingClientRect返回值
+  validIntersection(rectA, rectB) {
+    let p1 = [rectA.left, rectA.top] //rectA左上角坐标
+    let p2 = [rectA.right, rectA.bottom] //rectA右下角坐标
+    let p3 = [rectB.left, rectB.top] //rectB左上角坐标
+    let p4 = [rectB.right, rectB.bottom] //rectB右下角坐标
+    if (p2[1] > p3[1] && p1[1] < p4[1] && p2[0] > p3[0] && p1[0] < p4[0]) {
+      return true
+    }
+    return false
+  }
+
+  select() {
+    let rectArea = this.area.getBoundingClientRect()
+    this.selectData = []
+    this.selectItem.forEach((item) => {
+      let rectItem = item.getBoundingClientRect()
+      if (this.validIntersection(rectArea, rectItem)) {
+        this.selectData.push(item.dataset[this.dataKey])
+        item.style.background = '#6c5ce7'
+        item.style.color = '#fff'
+      } else {
+        item.style.background = '#fff'
+        item.style.color = '#000'
+      }
+    })
+    this.selectCallback()
+  }
 }
--- src/views/SelectArea.vue
+++ src/views/SelectArea.vue
@@ -1,6 +1,10 @@
 <template>
+  <div class="selectData">
+    selectData:
+    <span v-for="i in selectData" :key="i">{{ i }}</span>
+  </div>
   <div class="con" ref="con">
-    <div class="item" v-for="i in 40" :key="i">
+    <div class="item" v-for="i in 44" :key="i" :data-id="i">
       {{ i }}
     </div>
   </div>
@@ -8,33 +12,38 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
+import { SA } from '@/utils/index'
 const con = ref()
+const selectData = ref('[]')
 onMounted(() => {
-  console.log(con.value)
-  con.value.addEventListener('click', () => {
-    console.log('click')
+  let conDom = con.value
+  let items = document.querySelectorAll('.con .item')
+  const sa = new SA(conDom, items, 'id', () => {
+    selectData.value = `[ ${sa.selectData.join(', ')} ]`
   })
 })
 </script>
 
 <style lang="less" scoped>
 @size: 100px;
+.selectData {
+  padding: 20px 20px;
+  font-size: 24px;
+}
 .con {
-  border: 2px solid red;
-  padding: 40px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
+  border: 2px solid #000;
+  padding: 20px;
+  user-select: none;
 }
 .item {
-  width: @size;
-  height: @size;
-  border: 2px solid #000;
-  display: flex;
+  display: inline-flex;
   justify-content: center;
   align-items: center;
+  width: @size;
+  height: @size;
+  margin: 10px;
+  border: 4px solid #000;
   font-size: 28px;
   user-select: none;
-  // margin: 10px;
 }
 </style>
