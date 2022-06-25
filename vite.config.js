c93d9aa7a22ddb8e791861282d62f3d15101c394
YuFengjie97 <2457522528@qq.com>
2022-06-25 21:55:34 +0800

    feat: vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: 'https://yufengjie97.github.io/cheems/',
+  base: '/cheems/',
   plugins: [vue()],
   resolve: {
     alias: {
