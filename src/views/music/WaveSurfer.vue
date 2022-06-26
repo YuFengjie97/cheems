892796d1a458df0e790117d632f951f1d3742cf5
YuFengjie97 <2457522528@qq.com>
2022-06-26 11:18:54 +0800

    wavesurfer loading

--- src/views/music/WaveSurfer.vue
+++ src/views/music/WaveSurfer.vue
@@ -8,26 +8,41 @@
       @stop="handleStop"
       style="margin-bottom: 20px"
     />
-    <div id="waveform"></div>
+    <div class="waveCon">
+      <div id="waveform"></div>
+      <el-icon class="loading" v-show="isLoading">
+        <Loading />
+      </el-icon>
+    </div>
   </div>
 </template>
 
 <script setup>
+import {
+  Loading,
+} from '@element-plus/icons-vue'
 import AudioControl from '@/components/audioControl/index.vue'
 import { onMounted, onUnmounted, ref } from 'vue'
 import * as WaveSurfer from 'wavesurfer.js'
 import openSun from '@/assets/audio/openSun.mp3'
 let wavesurfer = null
+const isLoading = ref(true)
 onMounted(() => {
   wavesurfer = WaveSurfer.create({
     container: '#waveform',
     waveColor: 'violet',
     progressColor: 'purple',
-    skipLength: 5
+    skipLength: 5,
+    barRadius: 5,
+    barWidth: 5,
   })
   wavesurfer.load(openSun)
+  wavesurfer.on('ready', () => {
+    console.log('load ok')
+    isLoading.value = false
+  })
 })
-onUnmounted(()=>{
+onUnmounted(() => {
   wavesurfer.destroy()
 })
 function handleBackward() {
@@ -47,4 +62,25 @@ function handleStop() {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.waveCon {
+  position: relative;
+}
+.loading {
+  font-size: 2rem;
+  color: rgb(22, 219, 246);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: rotate 2s linear infinite;
+}
+@keyframes rotate {
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+}
+</style>
