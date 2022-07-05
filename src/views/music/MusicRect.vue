25f0ea67824f903204378cdb46faf54f8bc14cea
Yu Fengjie <wb_yufengjie@meituan.com>
2022-07-05 10:32:17 +0800

    wtf f bro

--- src/views/music/MusicRect.vue
+++ src/views/music/MusicRect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="con">
+  <div class="con" ref="con">
     <AudioControl
       class="audioControl"
       :showStop="false"
@@ -18,39 +18,61 @@
 
 <script setup>
 import AudioControl from '@/components/audioControl/index.vue'
-import { Rect, colors, random, AudioAnalyser, getMax } from '@/utils'
-import { onMounted, ref } from 'vue'
+import { Rect, colors, random, AudioAnalyser, getMaxAndAvg } from '@/utils'
+import { watch, ref } from 'vue'
+import { useStore } from '@/store/index'
 import mp3 from '@/assets/audio/audio1.mp3'
 
+const store = useStore()
+
 let aa = null
+let isPlaying = false
+const audio = ref()
+const fftSize = 512
 
 let ctx = null
-const audio = ref()
+const con = ref()
 const canvas = ref()
 const canvasWidth = ref(window.innerWidth - 220)
 const canvasHeight = ref(window.innerHeight - 50)
-const fftSize = 512
-const rectNum = fftSize / 2 // 完整取样
+const rectNum = parseInt(fftSize / 2) // 完整取样
 const coe = canvasHeight.value / 255 // 高度变化系数,audioDataMax最大值是255
 const gap = 1
 const rects = []
-// let h = 1 //基准色色相
-let hScale = 1 // 色相取余比例
-let audioDataMax = 300 // audioData最大值
+let h = 1 //基准色色相
+// let audioDataMax = 300 // audioData最大值
 
-onMounted(() => {
+// onMounted(() => {
+
+//
+// })
+
+watch(
+  () => store.mainHeight,
+  () => {
+    init()
+  }
+)
+
+function init() {
   audio.value.src = mp3
+  initCanvas()
+  createRect()
+  updateRect()
+}
 
+function initCanvas() {
+  const { width } = con.value.getBoundingClientRect()
+  canvasWidth.value = width
+  canvasHeight.value = store.mainHeight - 50
   ctx = canvas.value.getContext('2d')
-  createRect(ctx)
-})
-
-function createRect(ctx) {
-  // ctx.save()
   // 让条形看上去是以底部为基准,高度往上增加
-  ctx.translate(canvasWidth.value, canvasHeight.value)
+  console.log(canvasWidth.value, canvasHeight.value)
   ctx.rotate(Math.PI)
+  ctx.translate(canvasWidth.value, canvasHeight.value)
+}
 
+function createRect() {
   const width = (canvasWidth.value - gap * (rectNum - 1)) / rectNum
 
   for (let i = 0; i < rectNum; i++) {
@@ -62,27 +84,22 @@ function createRect(ctx) {
 }
 
 function updateRect() {
-  // 色相会逐渐变化
-  // if (h < 360) {
-  //   h++
-  // } else {
-  //   h = 1
-  // }
-  if (hScale < 360) {
-    hScale++
+  if (!isPlaying) return
+  if (h < 360) {
+    h += 1
   } else {
-    hScale = 1
+    h = 1
   }
   // ctx.clearRect(0,0,canvasWidth.value,canvasHeight.value)
   ctx.fillStyle = '#2d3436'
   ctx.fillRect(0, 0, canvasWidth.value, canvasHeight.value)
-  let audioData = aa.getAudioData()
-  audioDataMax = getMax(audioData)
-  audioData.reverse()
+  const audioData = aa.getAudioData()
+  const { avg, max } = getMaxAndAvg(audioData)
+  // audioData.reverse() // 因为canvas进行了反转平移,所以这里数组倒序
   rects.forEach((item, i) => {
     item.height = audioData[i] * coe
-    item.fillStyle = `hsl(${audioData[i] % hScale},${
-      (audioData[i] / audioDataMax) * 100 + '%'
+    item.fillStyle = `hsl(${parseInt((audioData[i] / max) * 360) + avg},${
+      (audioData[i] / avg) * 100 + '%'
     },50%)` // 饱和度会根据当前值/最大值比例来计算
     item.draw()
   })
@@ -90,11 +107,14 @@ function updateRect() {
 }
 
 function handlePlay() {
+  isPlaying = true
   audio.value.play()
-  aa = new AudioAnalyser(audio.value, fftSize)
-  updateRect()
+  if (!aa) {
+    aa = new AudioAnalyser(audio.value, fftSize)
+  }
 }
 function handlePause() {
+  isPlaying = false
   audio.value.pause()
 }
 </script>
